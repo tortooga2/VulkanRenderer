30829e1b403dfd35b1879e771c1b3e8baf0f4dc4
Chase Nagle <50192480+tortooga2@users.noreply.github.com>
2025-01-05 14:03:28 -0800

    Something

--- main.cpp
+++ main.cpp
@@ -6,66 +6,53 @@
 #include <GLFW/glfw3.h>
 #define GLFW_EXPOSE_NATIVE_COCOA
 
-
-
 #define SCR_WIDTH 800
 #define SCR_HEIGHT 600
 
-
-std::vector<const char*> getRequiredExtensions() {
-        uint32_t glfwExtensionCount = 0;
-        const char** glfwExtensions;
-        glfwExtensions = glfwGetRequiredInstanceExtensions(&glfwExtensionCount);
-        std::vector<const char*> extensions;
+std::vector<const char *> getRequiredExtensions()
+{
+    uint32_t glfwExtensionCount = 0;
+    const char **glfwExtensions;
+    glfwExtensions = glfwGetRequiredInstanceExtensions(&glfwExtensionCount);
+    std::vector<const char *> extensions;
     std::cout << "GLFW required extensions: " << glfwExtensionCount << std::endl;
 
-        for(uint32_t i = 0; i < glfwExtensionCount; i++) {
-            std::cout << glfwExtensions[i] << std::endl;
-            extensions.push_back(glfwExtensions[i]);
-        }
+    for (uint32_t i = 0; i < glfwExtensionCount; i++)
+    {
+        std::cout << glfwExtensions[i] << std::endl;
+        extensions.push_back(glfwExtensions[i]);
+    }
 
-        extensions.push_back(VK_KHR_PORTABILITY_ENUMERATION_EXTENSION_NAME);
-        extensions.push_back("VK_KHR_get_physical_device_properties2");
+    extensions.push_back(VK_KHR_PORTABILITY_ENUMERATION_EXTENSION_NAME);
+    extensions.push_back("VK_KHR_get_physical_device_properties2");
 
-
-
-
-
-
-        return extensions;
+    return extensions;
 }
 
 VkInstance instance;
 
-bool createInstance(){
+bool createInstance()
+{
 
+    VkApplicationInfo appInfo{};
+    appInfo.sType = VK_STRUCTURE_TYPE_APPLICATION_INFO;
+    appInfo.pApplicationName = "Hello Triangle";
+    appInfo.applicationVersion = VK_MAKE_VERSION(1, 0, 0);
+    appInfo.pEngineName = "No Engine";
+    appInfo.engineVersion = VK_MAKE_VERSION(1, 0, 0);
+    appInfo.apiVersion = VK_API_VERSION_1_2;
 
+    VkInstanceCreateInfo createInfo{};
+    createInfo.sType = VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO;
+    createInfo.pApplicationInfo = &appInfo;
 
-        VkApplicationInfo appInfo{};
-        appInfo.sType = VK_STRUCTURE_TYPE_APPLICATION_INFO;
-        appInfo.pApplicationName = "Hello Triangle";
-        appInfo.applicationVersion = VK_MAKE_VERSION(1, 0, 0);
-        appInfo.pEngineName = "No Engine";
-        appInfo.engineVersion = VK_MAKE_VERSION(1, 0, 0);
-        appInfo.apiVersion = VK_API_VERSION_1_2;
+    createInfo.flags |= VK_INSTANCE_CREATE_ENUMERATE_PORTABILITY_BIT_KHR;
 
-        VkInstanceCreateInfo createInfo{};
-        createInfo.sType = VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO;
-        createInfo.pApplicationInfo = &appInfo;
-
-        createInfo.flags |= VK_INSTANCE_CREATE_ENUMERATE_PORTABILITY_BIT_KHR;
-
-
-        auto extensions = getRequiredExtensions();
-        createInfo.enabledExtensionCount = static_cast<uint32_t>(extensions.size());
-        createInfo.ppEnabledExtensionNames = extensions.data();
-        return vkCreateInstance(&createInfo, nullptr, &instance) == VK_SUCCESS;
-
-
-
-
-    }
-
+    auto extensions = getRequiredExtensions();
+    createInfo.enabledExtensionCount = static_cast<uint32_t>(extensions.size());
+    createInfo.ppEnabledExtensionNames = extensions.data();
+    return vkCreateInstance(&createInfo, nullptr, &instance) == VK_SUCCESS;
+}
 
 int main()
 {
@@ -78,7 +65,6 @@ int main()
 
     glfwInitVulkanLoader(vkGetInstanceProcAddr);
 
-
     GLFWwindow *window = glfwCreateWindow(SCR_WIDTH, SCR_HEIGHT, "OpenGLProject", NULL, NULL);
     if (window == NULL)
     {
@@ -87,12 +73,10 @@ int main()
         return -1;
     }
 
-
-    if(createInstance()){
-        std::cout << "Something" << std::endl;
+    if (createInstance())
+    {
+        std::cout << "Vulkan is working!" << std::endl;
     };
-
-
 
     while (!glfwWindowShouldClose(window))
     {
