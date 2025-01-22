371b62780d35431e36a77b643b3e51fa9b7b9f37
chasen2021 <50192480+tortooga2@users.noreply.github.com>
2025-01-21 17:21:45 -0800

    Updated the Build script, as well as allowed Uniforms to be accessable from both Vertex and Fragment stages

--- run.py
+++ run.py
@@ -4,19 +4,17 @@ import platform
 cmake_command = "cmake --build ./build"
 run_command = ".\\build\\Debug\\VulkanEngine.exe"
 glslc_path = "C:\\VulkanSDK\\1.3.268.0\\Bin\\glslc.exe"
-
-
-if platform.system() == "Windows":
-    os.system(f"{glslc_path} ./Shaders/src/shader.vert -o ./shaders/compiled/vert.spv")
-    os.system(f"{glslc_path} ./Shaders/src/shader.frag -o ./shaders/compiled/frag.spv")
+    
 
 
 if platform.system() == "Darwin":
+    glslc_path = "/Users/chasenagle/VulkanSDK/1.3.296.0/macOS/bin/glslc"
     cmake_command = "cmake --build ./build"
     run_command = "./build/VulkanEngine"
     
 
-
+os.system(f"{glslc_path} ./Shaders/src/shader.vert -o ./shaders/compiled/vert.spv")
+os.system(f"{glslc_path} ./Shaders/src/shader.frag -o ./shaders/compiled/frag.spv")
 os.system(cmake_command)
 os.system(run_command)
 
